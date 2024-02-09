56e8c23b44c81e8badf63c344ba5d89f61e1744d
melnikovknst <145424645+melnikovknst@users.noreply.github.com>
2024-02-09 19:56:34 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+#include "tree.h"
+
+
+int main(void) {
+    int buf;
+    int n;
+    scanf("%d", &n);
+    
+    if (n == 0) {
+        printf("%d", n);
+        return 0;
+    }
+    
+    scanf("%d", &buf);
+    TREE* root = init(buf);
+    
+    for (int i = 1; i < n; i++) {
+        scanf("%d", &buf);
+        root = add(root, buf);
+    }
+    
+    printf("\nTree height: %d\n", root->height);
+    puts("-----");
+    ordered_print(root);
+    
+//    Searching element
+    int elem;
+    puts("\nEnter element for searching");
+    scanf("%d", &elem);
+    search(elem, root);
+    
+    del(root);
+    
+    return 0;
+}
